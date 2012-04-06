57e06651cefe4a26b93c98a7c2d395fc2eda2310
Joseph Perla <noootreeeeall@**jperla.com**>
2012-04-06 03:53:00 -0400

    nested expressions and other fun stuff

--- pyparse.py
+++ pyparse.py
@@ -1,3 +1,5 @@
+import string
+
 class ParseException(Exception):
     pass
 
@@ -9,7 +11,7 @@ class Join(ASTNode):
         self.name = name
         self.nodes = nodes
 
-    def parse(self, s):
+    def parse(self, s, whole=False):
         values = []
         remaining = s
         for n in self.nodes:
@@ -18,24 +20,51 @@ class Join(ASTNode):
                 values.append(value)
             else:
                 return None, s
-        return ((self.name,) + tuple(values)), remaining
+        if len(remaining) == 0 or not whole:
+            if self.name is not None:
+                return ((self.name,) + tuple(values)), remaining
+            else:
+                return tuple(values), remaining
+        else:
+            return None, s
+
+
+class Keyword(ASTNode):
+    def __init__(self, keyword, name=None):
+        self.keyword = keyword
+        self.name = name
+
+    def parse(self, s, whole=False):
+        #TODO: jperla: make whole decorator
+        if s[:len(self.keyword)] == list(self.keyword):
+            remaining = s[len(self.keyword):]
+            if self.name is None:
+                return self.keyword, remaining
+            else:
+                return (self.name, self.keyword), remaining
+        else:
+            return None, s
 
 class Symbol(ASTNode):
     def __init__(self, value):
         self.name = value
 
-    def parse(self, s):
-        if len(s) > 0 and s[0] == self.name:
-            return self.name, s[1:]
-        else:
+    def parse(self, s, whole=False):
+        if len(s) != 1:
             return None, s
+        else:
+            if len(s) > 0 and s[0] == self.name:
+                return self.name, s[1:]
+            else:
+                return None, s
 
 class ContiguousSymbols(ASTNode):
     def __init__(self, name, symbols):
         self.name = name
         self.symbols = symbols
 
-    def parse(self, s):
+    def parse(self, s, whole=False):
+        #TODO: jperla: ignores whole argument
         i = 0
         while i < len(s) and s[i] in self.symbols:
             i += 1
@@ -44,11 +73,23 @@ class ContiguousSymbols(ASTNode):
         else:
             return (self.name, ''.join(s[:i])), s[i:]
 
+class AlphaWord(ContiguousSymbols):
+    def __init__(self):
+        ContiguousSymbols.__init__(self, 'WORD', string.ascii_letters + '.')
+
+    def parse(self, s, whole=False):
+        value,remaining = ContiguousSymbols.parse(self, s)
+        if value is not None and (len(remaining) == 0 or not whole):
+            n,v = value
+            return v, remaining
+        else:
+            return None, s
+
 class Int(ContiguousSymbols):
     def __init__(self):
         ContiguousSymbols.__init__(self, 'Num', '0123456789')
 
-    def parse(self, s):
+    def parse(self, s, whole=False):
         value,remaining = ContiguousSymbols.parse(self, s)
         if value is not None:
             n,v = value
@@ -56,6 +97,53 @@ class Int(ContiguousSymbols):
         else:
             return None, s
 
+class Nested(ASTNode):
+    def __init__(self, open_='(', close=')', content=None, ignore=None):
+        self.open_ = open_
+        self.close = close
+        self.content = content
+        self.ignore = ignore
+
+    def parse(self, s, whole=False):
+        # do not do content ignoring (doesn't support parentheses in quotes)
+        assert self.ignore == None
+        stack = []
+        if len(s) > 0 and s[0] == self.open_:
+            stack = [s[0]]
+            i = 1
+
+            # nested expression, (e.g. look for matching parenthesis)
+            while len(stack) > 0 and i < len(s):
+                if s[i] == self.open_:
+                    stack.append(s[i])
+                elif s[i] == self.close:
+                    stack.pop()
+                i += 1
+
+            if len(stack) == 0:
+                inner_tokens, remaining = s[1:i-1], s[i:]
+                if self.content is not None:
+                    inner_ast, r = self.content.parse(inner_tokens, whole=False)
+                    if len(r) == 0:
+                        # nested expression found, and 
+                        # inner content parsed
+                        return inner_ast, remaining
+                    else:
+                        # inner content of nested expression
+                        # does not parse to self.content
+                        return None, s
+                else:
+                    # no content parser, just return raw string/tokens
+                    return inner_tokens, remaining
+            else:
+                # nested expression did not close (e.g. no close parenthesis)
+                return None, s
+        else:
+            # empty string, or first character doesn't open nested expression
+            # (e.g. no open parenthesis at beginning)
+            return None, s
+            
+        
 
 class Any(ASTNode):
     def __init__(self, *nodes):
@@ -70,6 +158,22 @@ class Any(ASTNode):
         else:
             return None, s
 
+class Repeat(ASTNode):
+    def __init__(self, parser):
+        self.parser = parser
+
+    def parse(self, s, whole=False):
+        parsed = []
+        remaining = s
+        while remaining is None or len(remaining) > 0:
+            ast, remaining = self.parser.parse(remaining, whole=False)
+            if ast is not None:
+                parsed.append(ast)
+            else:
+                break
+        return tuple(parsed), remaining
+
+
 class Recursive(ASTNode):
     def update(self, name, p):
         self.name = name
@@ -78,7 +182,10 @@ class Recursive(ASTNode):
     def parse(self, s, whole=False):
         value,remaining = self.parser.parse(s, whole)
         if value is not None:
-            return (self.name, value), remaining
+            if self.name is not None:
+                return (self.name, value), remaining
+            else:
+                return value, remaining
         else:
             return None, s
 
@@ -92,7 +199,7 @@ def whitespace_tokenize(s):
 
         Removes all whitespace and leaves just a stream of characters.
     """
-    return [c for c in s if c != ' ']
+    return [c for c in s if c not in ' \r\n']
 
 # todo: nested parentheses / brackets etc
 
